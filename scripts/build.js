3270137fe6a82aa5d15bf9e8b9ee2a66bbf036ee
zargovv <zargovv@gmail.com>
2023-11-24 21:52:04 +0600

    Tweak build script

--- scripts/build.js
+++ scripts/build.js
@@ -1,8 +1,10 @@
 // @ts-check
 
-const { cpSync, writeFileSync, readFileSync } = require("fs");
+const { cpSync, writeFileSync, readFileSync, mkdirSync } = require("fs");
+const { dirname } = require("path");
 const { deflateRawSync } = require("zlib");
 
+mkdirSync(dirname(process.argv[3]), { recursive: true });
 cpSync(process.argv[2], process.argv[3]);
 writeFileSync(
   process.argv[3] + ".dfl",
